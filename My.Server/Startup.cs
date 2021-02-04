8bb5aef7ac063ff9793d4ac44fe489233aec366f
Ronjun Cajilig <cajilig.rjc@gmail.com>
2021-02-04 11:15:10 +0800

    -Add Cors config

--- My.Server/Startup.cs
+++ My.Server/Startup.cs
@@ -32,6 +32,7 @@ namespace My.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -72,11 +73,14 @@ namespace My.Server
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                );
+            app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
